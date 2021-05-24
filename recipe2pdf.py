2507f8ba8c85615f7269b9eb36b125c3d0cdd6b6
thoras-stuff <thoras.stuff@protonmail.com>
2021-05-24 11:27:28 -0400

    pdf creation working

--- recipe2pdf.py
+++ recipe2pdf.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 
+import os
 import re
 import sys
 import subprocess
+import tempfile
+from pathlib import Path
+
 
 import attr
 from attr import attrs, attrib
@@ -13,6 +17,7 @@ E = Environment()
 @attrs
 class Recipe:
     title = attrib()
+    category = attrib()
     preamble = attrib()
     ingredients = attrib()
     directions = attrib()
@@ -27,20 +32,35 @@ def md2recipe(filepath):
     preamble = m.groups()[0].strip()
     ingredients = m.groups()[1].strip()
     directions = m.groups()[2].strip()
+    
 
     return Recipe(
         title=title,
+        category=filepath.parent,
         preamble=preamble,
         ingredients=ingredients,
         directions=directions
     )
 
-def recipe2pdf(recipe):
+    
+
+def recipe2tex(recipe):
     with open("recipe.tex.j2") as f:
         t = E.from_string(f.read())
-    print(t.render(recipe=recipe))    
+    return t.render(recipe=recipe)
 
+
+def tex2pdf(name, category, tex):
+    tex_dir = Path("pdf") / Path(category)
+    if not tex_dir.exists():
+        os.makedirs(tex_dir)
+    tex_path = tex_dir / Path(name)
+    with open(tex_path, "w") as f:
+        f.write(tex)    
+    subprocess.run(["pdflatex", name], cwd=tex_dir.absolute())
 
 if __name__ == "__main__":
-    r = md2recipe(sys.argv[1])
-    recipe2pdf(r)
+    p = Path(sys.argv[1])    
+    r = md2recipe(p)
+    tex = recipe2tex(r)
+    tex2pdf(p.name.replace(".md", ".tex"), r.category, tex)
